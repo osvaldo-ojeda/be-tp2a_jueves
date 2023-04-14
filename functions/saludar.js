155eedae5a789f9a0aff496f084aba716f99988d
Osvaldo Ojeda <osvaldo.devmate@gmail.com>
2023-04-13 22:46:26 -0300

    clase4

--- functions/saludar.js
+++
@@ -1,9 +0,0 @@
-function saludar() {
-   console.log("Buenos nachos Argentina!!!!!")
-}
-function chau() {
-    console.log("Chauuuuuuuu!!!!!")
-}
-
-// module.exports={saludar,chau}
-module.exports=saludar
